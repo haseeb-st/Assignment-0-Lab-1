f4a8e2e51675f7d6ebfff59d6b4b2b92161afba9
687159 <687159@elf7it216372.ACDM.DS.SAIT.CA>
2017-09-12 13:23:55 -0600

    New Project Creation Created a new Java Application for HelloWorld.
    Updates:
    V1: Added "Is this thing on...?" line.
    V2: Added "It does seem to be on!" line.
    V3: Added comments to the program.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Haseeb Sheikh
+ * 000687159
+ * Assignment 0
  */
 package helloworld;
 
@@ -13,6 +13,7 @@ public class HelloWorld
 {
 
     /**
+     * Code for HelloWorld found below.
      * @param args the command line arguments
      */
     public static void main(String[] args) 
