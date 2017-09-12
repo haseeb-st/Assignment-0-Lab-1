650618723e04aba55fa75aca0ef61eafda42f908
687159 <687159@elf7it216372.ACDM.DS.SAIT.CA>
2017-09-12 13:18:12 -0600

    New Project Creation Created a new Java Application for HelloWorld.
    Updates:
    
    V1: Added "Is this thing on...?" line.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -17,7 +17,8 @@ public class HelloWorld
      */
     public static void main(String[] args) 
     {
-        System.out.println ("Hello World");
+        System.out.println ("Hello World\n");
+        System.out.println ("\nIs this thing on...?");
         // TODO code application logic here
     }
     
